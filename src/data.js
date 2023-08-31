ea5782c1d2a3f2a8715bf67888d33de29670d64b
Hector Eligio <hectjr628@gmail.com>
2023-08-31 11:08:13 -0400

    First commit

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const projects = [
+    {
+        title: "react Reserve",
+        Subtitle: "MERN Stack",
+        Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-1.gif",
+        link: "https://reactbootcamp.com",
+
+    },
+    {
+        title: "react Tracks",
+        Subtitle: "React and Python",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-2.gif",
+        link: "https:/reedbarger.com"
+    }
+]
+
+export const skills = [
+
+]
+
+export const testimonials = [
+
+]
